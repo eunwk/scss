132a64a4e0bcb1fd18ed7a86e2e6cc2968450235
eunwk <eunwk@naver.com>
2024-05-29 14:51:19 +0900

    웹팩  적용실패

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import * as vars from './scss/common/_variables.scss';
+import { primary } from './scss/common/_variables.scss'
+
+console.log("웹팩실행", vars.primary, primary)
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              exportLocalsConvention: 'camelCase'
+            }
+          }
+        }, 'sass-loader']
+      },
+      {
+        test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+          	name: '[name].[ext]?[hash]'
+          }
+        }]
+      }
+  	]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: {
+      	collapseWhitespace: true
+      },
+      hash: true,
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })
+  ],
+  devServer: {
+    //contentBase: path.resolve(__dirname, 'public'),
+  	port: 9000
+  },
+}
