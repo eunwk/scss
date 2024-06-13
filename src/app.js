6b1ad24919d945e9ee6b96a4961455bb240da11a
eunwk <eunwk@naver.com>
2024-06-13 17:23:32 +0900

    assets loader 설정 변경. 및 테스트

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
-import * as vars from './assets/scss/common/_variables.scss';
-import { primary } from './assets/scss/common/_variables.scss'
-import './assets/css/styles.css';
+// import * as vars from './assets/scss/common/_variables.scss';
+// import { primary } from './assets/scss/common/_variables.scss'
+import './assets/scss/styles.scss';
+import ico from './assets/images/icon_edit.svg';
 
-console.log("웹팩실행", vars.primary, primary);
+// console.log("웹팩실행", vars.primary, primary);
 
 const checkViewportHeight = () => {
    // console.log("checkView")
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 };
- 
+const createImage = `<div>
+<img src="${ico}" />
+<img src="/images/arrow-alt-left2.png" width="50px"/>
+<img src="./assets/images/bg-dot.png" width="50px"/>
+</div>
+
+`
 
 const init = () => {
     checkViewportHeight();
@@ -17,3 +24,7 @@ const init = () => {
 
 window.addEventListener("load", init)
 window.addEventListener('resize', checkViewportHeight);
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#root").innerHTML = createImage;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,61 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/app.js',
-  output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js'
+  resolve: {
+    extensions: ['.js'], 
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, {
           loader: 'css-loader',
           options: {
-            modules: {
-              exportLocalsConvention: 'camelCase'
-            }
-          }
-        }, 'sass-loader']
+              esModule: false, // url()을 올바르게 처리하기 위한 설정 (파일명이 바뀌지 않음.)
+          },
+        }],
       },
       {
-        test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|otf)?$/,
-        use: [{
-          loader: 'file-loader',
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader,  {
+          loader: 'css-loader',
           options: {
-          	name: '[name].[ext]?[hash]',
-          }
-        }]
+              esModule: false, // url()을 올바르게 처리하기 위한 설정 (파일명이 바뀌지 않음.)
+          },
+        }, 'sass-loader'],
       },
+      {
+        test: /\.(ico|png|jpg|jpeg|gif|svg)$/,
+        use: [{
+            loader:'file-loader',
+            options: {
+                outputPath: 'images',
+                name: '[name].[ext]'
+            }
+        }],
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]',
+            }
+        }]
+    },
      
   	]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename : "bundle.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -43,13 +63,31 @@ module.exports = {
       	collapseWhitespace: true
       },
       hash: true,
-      template: './src/index.html' // build 시 해당경로의 파일이 public에 생성됨
+      template: './public/index.html',   // build 시 해당경로의 파일이 dist 생성됨
     }),
-    new MiniCssExtractPlugin({
-      filename: './css/styles.css' // 루트 폴더 기준으로 빌드 되는 경로 localhost/css/styles.css
-    })
+   
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'public/',
+          to: '',  // dist 이후 경로
+          globOptions: {
+            ignore: ['**/*.html', '**/*.js'],
+          },
+        },
+      ],
+  }),
+  new MiniCssExtractPlugin({
+    filename: 'css/styles.css' // 루트 폴더 기준으로 빌드 되는 경로 localhost/css/styles.css
+  }),
+  new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+  }),
   ],
   devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+  },
   	port: 9000,
     hot: true
   },
